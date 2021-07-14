5b591ee799b7ae5380ff6737013d8d08f18d8f6f
Mohammed Zeeshan Ahmed <mohammed.zee1000@gmail.com>
2021-07-14 15:08:43 +0530

    Fixing condition
    Signed-off-by: Mohammed Zeeshan Ahmed <mohammed.zee1000@gmail.com>

--- pkg/odo/cli/service/create.go
+++ pkg/odo/cli/service/create.go
@@ -116,8 +116,8 @@ func (o *CreateOptions) Complete(name string, cmd *cobra.Command, args []string)
 	// use operator backend only
 	o.Backend = NewOperatorBackend()
 	// since interactive mode is not supported for Operators yet, set it to false
-	// if it is already true, we error out
-	if o.interactive || (o.fromFile == "" && len(args) != 1) {
+	// if it is already true, or from file is not provided and no args are provided either we error out
+	if o.interactive || (o.fromFile == "" && (len(args) != 1 && len(args) != 2)) {
 		return fmt.Errorf("odo doesn't support interactive mode for creating Operator backed service")
 	}
 	o.interactive = false
